243439fbddfb52e327363345e7ce5fef8a64d083
soyamamoto <kbc19b30@stu.kawahara.ac.jp>
2022-02-02 21:59:34 +0900

    敵にぶつかったときに残機が減るようにした

--- 2DShooting/Assets/Scripts/Enemy2Controller.cs
+++ 2DShooting/Assets/Scripts/Enemy2Controller.cs
@@ -32,6 +32,7 @@ public class Enemy2Controller : MonoBehaviour
     {
         if (coll.gameObject.tag == "Player")
         {
+            GameObject.Find("Canvas").GetComponent<UIController>().AddZanki();
             GameObject.Find("Canvas").GetComponent<UIController>().GameOver();
         }
     }
